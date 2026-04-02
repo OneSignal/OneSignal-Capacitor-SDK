85fd1191c4255c771e5aacd6579ba6d5873d0c36
Fadi George <fadii925@gmail.com>
2026-04-02 16:07:12 -0700

    build: add pre-commit hooks and lint config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite-plus';
 
 export default defineConfig({
+  staged: {
+    '*': 'vp check --fix',
+  },
   fmt: {
     singleQuote: true,
     sortImports: {
       enabled: true,
     },
+  },
+  lint: {
+    options: { typeAware: true, typeCheck: true },
   },
   pack: {
     entry: 'src/index.ts',
