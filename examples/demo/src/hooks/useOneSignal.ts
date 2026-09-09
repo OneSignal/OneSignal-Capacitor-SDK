4af60d52f06a670a3add148b1b076c9e981e4faa
Fadi George <fadii925@gmail.com>
2026-09-09 13:49:33 -0700

    chore(demo): improve debug log labels

--- examples/demo/src/hooks/useOneSignal.ts
+++ examples/demo/src/hooks/useOneSignal.ts
@@ -215,13 +215,16 @@ export function useOneSignal(): UseOneSignalReturn {
       console.log(`[OneSignal] Notification click: ${e.notification.title ?? ''}`);
 
       // uncomment to see the full event object
-      // console.log('[OneSignal] event: ', e);
+      // console.log('[OneSignal] click event: ', e);
     };
 
     const handleForegroundWillDisplay = (e: NotificationWillDisplayEvent) => {
       console.log(
         `[OneSignal] Notification foregroundWillDisplay: ${e.getNotification().title ?? ''}`,
       );
+
+      // uncomment to see the full event object
+      // console.log('[OneSignal] foregroundWillDisplay event: ', e);
 
       // uncomment to test preventing the default display behavior
       // e.preventDefault();
